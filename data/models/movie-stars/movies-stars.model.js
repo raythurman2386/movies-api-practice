6ef3e1c48371d8d55b2d1d88c30919c08a5fcf32
raythurman2386 <raymondthurman5@gmail.com>
2019-12-12 19:57:29 -0500

    moved models

---
+++ data/models/movie-stars/movies-stars.model.js
@@ -0,0 +1,18 @@
+const db = require("../../dbConfig");
+
+function findStarsBy(movie_id) {
+  return db("movies_stars").where({ movie_id });
+}
+
+async function addStarToMovie(starMovie) {
+  const [movie_id] = await db("movies_stars").insert(starMovie);
+  return findStarsBy(movie_id);
+}
+
+function removeStarFromMovie(movie_id, stars_id) {
+  return db("movies_stars")
+    .where({ movie_id, stars_id })
+    .del();
+}
+
+module.exports = { findStarsBy, addStarToMovie, removeStarFromMovie };
---
+++ data/models/movies/movies.model.js
@@ -0,0 +1,29 @@
+const db = require("../../dbConfig");
+
+function find() {
+  return db("movies");
+}
+
+function findBy(filter) {
+  return db("movies").where(filter);
+}
+
+async function addMovie(newMovie) {
+  const [id] = await db("movies").insert(newMovie);
+  return findBy({ id });
+}
+
+async function updateMovie(id, movie) {
+  await db("movies")
+    .where({ id })
+    .update(movie);
+  return findBy({ id });
+}
+
+function deleteMovie(id) {
+  return db("movies")
+    .where({ id })
+    .del();
+}
+
+module.exports = { find, findBy, addMovie, updateMovie, deleteMovie };
